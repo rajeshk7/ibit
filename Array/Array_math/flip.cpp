a6285c9b249f7a2faeae2aec46d9a90ff54b32a5
RAJESH KUMAR JHA <30970253+rajeshk7@users.noreply.github.com>
2018-12-15 20:26:14 +0530

    Create flip.cpp

---
+++ Array/Array_math/flip.cpp
@@ -0,0 +1,9 @@
+vector<int> Solution::flip(string A) {
+    vector<int> res;
+    if(A.find("0") == A.end)
+        return res;
+    int max 
+    for( int i = 0 ; i < A.size() ; i++ )   {
+        
+    }
+}
